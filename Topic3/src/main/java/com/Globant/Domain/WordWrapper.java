a036c42af198e08df0b4f146697ab356f51d7ccf
andresm96 <andres-malvestiti@hotmail.com>
2017-08-04 12:36:46 -0300

    ex 2

---
+++ Topic3/src/main/java/com/Globant/Domain/WordWrapper.java
@@ -0,0 +1,43 @@
+package com.Globant.Domain;
+
+public class WordWrapper {
+
+	public String breakSentence(String sentence, int segment) {
+
+		String text = "";
+		if (sentence.length() <= segment) {
+			return sentence;
+		} else {
+
+			int infLimit = 0;
+			int supLimit = segment;
+			String token;
+
+			do {
+				token = sentence.substring(infLimit, supLimit);
+				int space = token.indexOf(' ') + infLimit;
+				if (space != -1) {
+					token = sentence.substring(infLimit, space);
+					text += token + "\n";
+					infLimit = space + 1;
+					supLimit += segment;
+					if (supLimit > sentence.length())
+						break;
+					/*
+					 * token = sentence.substring(infLimit, supLimit); text +=
+					 * token + "\n";
+					 */
+				} else {
+					token = sentence.substring(infLimit, supLimit);
+					text += token + "\n";
+					infLimit = supLimit;
+					supLimit += segment;
+				}
+			} while (supLimit <= sentence.length());
+			token = sentence.substring(infLimit, sentence.length());
+			text += token;
+			return text;
+		}
+	}
+
+}
